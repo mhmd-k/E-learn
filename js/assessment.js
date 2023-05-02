42d4e7d5092f12ac3792b741db36404612480c77
mhmd-k <mhmdk425@gmail.com>
2023-05-02 09:04:11 +0300

    finish the course page and the assessment section

---
+++ js/assessment.js
@@ -0,0 +1,123 @@
+const startSection = document.querySelector("section.start");
+const quizSection = document.querySelector("section.quiz");
+const endGameSection = document.querySelector("section.end");
+const nextBtn = document.getElementById("submit");
+const startBtn = document.getElementById("start");
+const restartBtn = document.getElementById("restart");
+
+let score = 0;
+let questionIndex = 0;
+let questions;
+let interval;
+
+startBtn.addEventListener("click", () => {
+  startSection.classList.add("d-none");
+  quizSection.classList.remove("d-none");
+  getQuestions().then((data) => {
+    questions = data;
+    displayQuestion(questions[questionIndex]);
+  });
+});
+
+nextBtn.addEventListener("click", () => {
+  let userHasChoose = false;
+  document.querySelectorAll("input[type='radio']").forEach((e) => {
+    if (e.checked) {
+      userHasChoose = true;
+    }
+  });
+  if (!userHasChoose) {
+    return;
+  }
+  clearInterval(interval);
+  submit(questions[questionIndex]);
+});
+
+restartBtn.addEventListener("click", () => {
+  window.location.reload();
+});
+
+async function getQuestions() {
+  const res = await fetch(`../assessments/${course.name}.json`);
+  const questions = await res.json();
+  return questions;
+}
+
+function displayQuestion(q) {
+  quizSection.querySelector("ul.answers").innerHTML = "";
+  quizSection.querySelector("h2").innerText = q.title;
+  quizSection.querySelector("h4").innerHTML = `Question ${
+    questionIndex + 1
+  } out of 10`;
+  for (let i in q.answers) {
+    const li = document.createElement("li");
+    const label = document.createElement("label");
+    label.setAttribute("for", q.answers[i]);
+    label.innerText = q.answers[i];
+    const radio = document.createElement("input");
+    radio.setAttribute("type", "radio");
+    radio.id = q.answers[i];
+    radio.name = "answer";
+    radio.value = q.answers[i];
+    li.append(radio, label);
+    quizSection.querySelector("ul.answers").append(li);
+  }
+  timer();
+}
+
+function checkAnswer(correctAnswer) {
+  let correct = false;
+  quizSection.querySelectorAll("input[type='radio']").forEach((e) => {
+    if (e.checked && correctAnswer === e.value) {
+      correct = true;
+    }
+  });
+  if (correct) {
+    score = score + 10;
+  }
+}
+
+function finish() {
+  quizSection.classList.add("d-none");
+  endGameSection.classList.remove("d-none");
+  if (score >= 60) {
+    endGameSection.querySelector("h2").innerHTML = `
+        You have passed <br /> Your Score is: ${score} out of 100`;
+    endGameSection
+      .querySelector("img")
+      .setAttribute(
+        "src",
+        "./images/GraveNegativeHarpseal-size_restricted.gif"
+      );
+  } else {
+    endGameSection.querySelector("h2").innerHTML = `
+        YOU didn't pass <br /> Your Score is: ${score} out of 100 <br /> Score 60 or higher to pass`;
+    endGameSection
+      .querySelector("img")
+      .setAttribute("src", "./images/giphy.gif");
+  }
+}
+
+function submit(question) {
+  checkAnswer(question.right_answer);
+  questionIndex++;
+  if (questionIndex === 10) {
+    finish();
+  } else {
+    displayQuestion(questions[questionIndex]);
+  }
+}
+
+function timer() {
+  quizSection.querySelector("h4.timer").innerHTML = "00:30";
+  let seconds = 30;
+  interval = setInterval(() => {
+    seconds--;
+    let str = seconds < 10 ? `00:0${seconds}` : `00:${seconds}`;
+    quizSection.querySelector("h4.timer").innerHTML = str;
+    if (seconds === 0) {
+      clearInterval(interval);
+      submit(questions[questionIndex]);
+    }
+  }, 1000);
+}
--- js/course.js
+++ js/course.js
@@ -1,40 +1,240 @@
 const courses = [
   {
     id: 1,
-    image: "/images/html.png",
+    name: "html",
     title: "HTML5 crash course",
-    time: 2,
+    leassons: [
+      "https://www.youtube.com/embed/6QAELgirvjs",
+      "https://www.youtube.com/embed/7LxA9qXUY5k",
+      "https://www.youtube.com/embed/QG5aEmS9Fu0",
+      "https://www.youtube.com/embed/dVgTBEYCseU",
+    ],
   },
   {
     id: 2,
-    image: "/images/java.jpg",
+    name: "java",
     title: "Java Programming and Software Engineering Fundamentals",
-    time: 13,
+    leassons: [
+      "https://www.youtube.com/embed/mNvJipMTKSM",
+      "https://www.youtube.com/embed/wJKgqYj4mtU",
+      "https://www.youtube.com/embed/vtHwpyk5v08",
+      "https://www.youtube.com/embed/mY8yxPP9_AE",
+    ],
   },
   {
     id: 3,
-    image: "/images/linux.jpg",
+    name: "linux",
     title: "Linux Fundamentals",
-    time: 8,
+    leassons: [
+      "https://www.youtube.com/embed/YHFzr-akOas",
+      "https://www.youtube.com/embed/dQ8JgDUS8DA",
+      "https://www.youtube.com/embed/FTTr2bjI2UM",
+      "https://www.youtube.com/embed/E01hskdRmUg",
+    ],
   },
   {
     id: 4,
-    image: "/images/nodejs.jpg",
+    name: "nodejs",
     title: "Node.js Backend Basics with Best Practices",
-    time: 5,
+    leassons: [
+      "https://www.youtube.com/embed/zb3Qk8SG5Ms",
+      "https://www.youtube.com/embed/OIBIXYLJjsI",
+      "https://www.youtube.com/embed/-HPZ1leCV8k",
+      "https://www.youtube.com/embed/DQD00NAUPNk",
+    ],
   },
   {
     id: 5,
-    image: "/images/sql.jpg",
+    name: "sql",
     title: "Introduction to Structured Query Language (SQL)",
-    time: 4,
+    leassons: [
+      "https://www.youtube.com/embed/DftlOK7fCtc",
+      "https://www.youtube.com/embed/z4j4S6GsxaU",
+      "https://www.youtube.com/embed/9p7ugFU9dvs",
+      "https://www.youtube.com/embed/9p7ugFU9dvs",
+    ],
   },
   {
     id: 6,
-    image: "/images/react.png",
+    name: "react",
     title: "React Basics",
-    time: 5,
+    leassons: [
+      "https://www.youtube.com/embed/j942wKiXFu8",
+      "https://www.youtube.com/embed/kVeOpcw4GWY",
+      "https://www.youtube.com/embed/9D1x7-2FmTA",
+      "https://www.youtube.com/embed/pnhO8UaCgxg",
+    ],
   },
 ];
+
 const course = courses[parseInt(localStorage.getItem("course")) - 1];
-console.log(course);
+const test = document.querySelector("#test");
+const aside = document.querySelector("aside");
+const asideBtn = document.querySelector("header #aside");
+const leasonBtns = document.querySelectorAll("li.leason");
+const project = document.querySelector("#project");
+const mainContainer = document.querySelector(".content");
+
+// show and hide aside in small screens
+let show = false;
+aside.addEventListener("click", (e) => e.stopPropagation());
+asideBtn.addEventListener("click", (e) => {
+  e.stopPropagation();
+  if (show) {
+    show = false;
+    aside.classList.remove("active");
+  } else {
+    show = true;
+    aside.classList.add("active");
+  }
+});
+document.addEventListener("click", () => {
+  show = false;
+  aside.classList.remove("active");
+});
+
+// update the content of the intro screen
+mainContainer.innerHTML = `
+  <p class="text-muted">${course.title} <span>> ${leasonBtns[0].textContent}</span></p>
+  <iframe
+    src="${course.leassons[0]}"
+    title="YouTube video player"
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    allowfullscreen
+  ></iframe>
+  <p>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, quis
+    nesciunt suscipit aperiam officiis perferendis vitae distinctio
+    consequatur. Alias placeat nemo ex velit error consectetur tempora
+    maiores, expedita autem non. Sequi corrupti recusandae odit dolor,
+    distinctio nulla rerum animi perspiciatis minima amet illum quos ea et
+    provident labore eum quisquam sit repellendus odio mollitia inventore
+    dicta iure? Nostrum, rerum eius? Quam possimus ea rerum omnis fugiat
+    modi. xpedita autem non. Sequi corrupti recusandae odit dolor,
+    distinctio nulla rerum animi perspiciatis minima amet illum quos ea et
+    provident labore eum quisquam sit repellendus odio mollitia inventore
+    dicta iure
+  </p>
+`;
+
+leasonBtns.forEach((e, i) => {
+  e.addEventListener("click", () => {
+    loadingAnimation();
+    mainContainer.classList.add("loading");
+    setTimeout(() => {
+      mainContainer.classList.remove("loading");
+      mainContainer.innerHTML = `
+        <p class="text-muted">${course.title} <span>> ${e.textContent}</span></p>
+        <iframe
+          src="${course.leassons[i]}"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, quis
+          nesciunt suscipit aperiam officiis perferendis vitae distinctio
+          consequatur. Alias placeat nemo ex velit error consectetur tempora
+          maiores, expedita autem non. Sequi corrupti recusandae odit dolor,
+          distinctio nulla rerum animi perspiciatis minima amet illum quos ea et
+          provident labore eum quisquam sit repellendus odio mollitia inventore
+          dicta iure? Nostrum, rerum eius? Quam possimus ea rerum omnis fugiat
+          modi. xpedita autem non. Sequi corrupti recusandae odit dolor,
+          distinctio nulla rerum animi perspiciatis minima amet illum quos ea et
+          provident labore eum quisquam sit repellendus odio mollitia inventore
+          dicta iure
+        </p>`;
+    }, 1000);
+    document
+      .querySelectorAll("aside ul li")
+      .forEach((li) => li.classList.remove("active"));
+    e.classList.add("active");
+  });
+});
+
+project.addEventListener("click", () => {
+  loadingAnimation();
+  mainContainer.classList.add("loading");
+  setTimeout(() => {
+    mainContainer.classList.remove("loading");
+    mainContainer.innerHTML = `
+        <p class="text-muted">${course.title}  <span>> ${project.textContent}</span></p>
+        <h3>What is the purpose of this project?</h3>
+        <p>The primary purpose of this project is
+            to check your knowledge and understanding of the key
+            learning objectives of the course you have just completed.
+            Most importantly, projects help you establish which topics
+            you have mastered and which require further focus before 
+            completing the course. Ultimately, the project is designed to help you
+            make sure that you can apply what you have learned.
+        </p>
+        <div class="upload">
+            <h3>Upload your project</h3>
+            <input type="file" name="upload" />
+        </div>
+    `;
+  }, 1000);
+  document
+    .querySelectorAll("aside ul li")
+    .forEach((li) => li.classList.remove("active"));
+  project.classList.add("active");
+});
+
+function loadingAnimation() {
+  mainContainer.innerHTML = `
+        <div class="loadingio-spinner-rolling-fvxxpi27vlr"><div class="ldio-h7qmz13llzu">
+            <div class="course"></div>
+        </div></div>`;
+}
+
+test.addEventListener("click", () => {
+  loadingAnimation();
+  mainContainer.classList.add("loading");
+  document
+    .querySelectorAll("aside ul li")
+    .forEach((li) => li.classList.remove("active"));
+  test.classList.add("active");
+  setTimeout(() => {
+    mainContainer.classList.remove("loading");
+    const subject = course.name.toUpperCase();
+    mainContainer.innerHTML = `
+    <!-- start section -->
+    <section class="start">
+        <h3>${subject} ASSESSMENT</h3>
+        <p>
+          Score 60 or higher to pass.<br />
+          You have 30 seconds to answer each question. <br />
+          good luck.
+        </p>
+        <button id="start">START</button>
+    </section>
+    <!-- quiz section -->
+    <section class="quiz d-none">
+        <div class="top">
+          <h4></h4>
+          <h4 class="timer">00:30</h4>
+        </div>
+        <h2></h2>
+        <ul class="answers"></ul>
+        <button id="submit">
+          Next <i class="fa-solid fa-arrow-right"></i>
+        </button>
+    </section>
+    <!-- lose/win section -->
+    <section class="end d-none">
+        <img src="" alt="" />
+        <h2></h2>
+        <button id="restart">
+          Restart <i class="fa-solid fa-arrow-rotate-left"></i>
+        </button>
+    </section>
+    `;
+
+    // assessment
+    const assessment = document.createElement("script");
+    assessment.src = "../js/assessment.js";
+    document.body.appendChild(assessment);
+  }, 1000);
+});
